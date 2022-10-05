624d68f5ad34f7f4a3097b306674ad548550bff4
Lucas Frederico Roeder de Mello <104468335+lucasmellofred@users.noreply.github.com>
2022-10-05 14:23:12 -0300

    Update sjf.js

--- sjf.js
+++ sjf.js
@@ -9,9 +9,9 @@ let listaPriorizada = [];
 
   function geraTempo(){
     let neg = 0;
-    for(let i = 0; i < lista.length; i++){
-      for(let x = 0; x < lista[i].carga.length; x++){
-        if(lista[i].carga[x] < 0){
+    for (let i = 0; i < lista.length; i++){
+      for (let x = 0; x < lista[i].carga.length; x++){
+        if (lista[i].carga[x] < 0){
           neg = lista[i].carga[x] < -1 ? neg + neg + 2 : neg = neg + 1;
         }
       }
@@ -31,22 +31,22 @@ let listaPriorizada = [];
 
 function realizaExec(){
   let cont = 0;
-  while(cont<3){
+  while (cont<3){
     cont++;
-    for(let i = 0; i < lista.length; i++){
-      for(let x = 0; x < lista[i].carga.length; x++){
-        if(lista[i].carga[0]>0){
+    for (let i = 0; i < lista.length; i++){
+      for (let x = 0; x < lista[i].carga.length; x++){
+        if (lista[i].carga[0]>0){
           lista[i].carga.splice(0, 1);
-        }else{
+        } else {
           lista[i].carga[0]++;
-          if(lista[i].carga[0] == 0){
+          if (lista[i].carga[0] == 0){
             lista[i].carga.splice(0, 1);
           }
-          for(let j = 0; j < i; j++){
-            for(let f = 0; f < lista[j].carga.length; f++){
-              if(lista[j].carga[0] < 0){
+          for (let j = 0; j < i; j++){
+            for (let f = 0; f < lista[j].carga.length; f++){
+              if (lista[j].carga[0] < 0){
                 lista[j].carga[0]++;
-                if(lista[j].carga[0] == 0){
+                if (lista[j].carga[0] == 0){
                   lista[j].carga.splice(0, 1);
                 }
               }
@@ -60,5 +60,3 @@ function realizaExec(){
 
 geraTempo();
 console.log(lista);
-  
-  
