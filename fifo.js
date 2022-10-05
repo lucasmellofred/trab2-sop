8dfb03f13f89d35f3cab607a6f6f519de5db4f84
Lucas Frederico Roeder de Mello <104468335+lucasmellofred@users.noreply.github.com>
2022-10-05 14:19:21 -0300

    Add files via upload

---
+++ fifo.js
@@ -0,0 +1,39 @@
+let lista = [
+  { id: 'p1', prioridade: 2, carga: [2,-1,1] },
+  { id: 'p2', prioridade: 1, carga: [1,-2,1,2] },
+  { id: 'p3', prioridade: 2, carga: [1,-1,-1,1] },
+  { id: 'p4', prioridade: 3, carga: [2,2,1,-1,2] },
+];
+
+//FIFO
+let continua = true;
+let cont = 0;
+while(cont<3){
+  cont++;
+  for(let i = 0; i < lista.length; i++){
+    for(let x = 0; x < lista[i].carga.length; x++){
+      if(lista[i].carga[0]>0){
+        lista[i].carga.splice(0, 1);
+      }else{
+        console.log('negativo -- '+lista[i].carga[0]);
+        lista[i].carga[0]++;
+        if(lista[i].carga[0] == 0){
+          lista[i].carga.splice(0, 1);
+        }
+        for(let j = 0; j < i; j++){
+          for(let f = 0; f < lista[j].carga.length; f++){
+            if(lista[j].carga[0] < 0){
+              lista[j].carga[0]++;
+              if(lista[j].carga[0] == 0){
+                lista[j].carga.splice(0, 1);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
+console.log(lista);
